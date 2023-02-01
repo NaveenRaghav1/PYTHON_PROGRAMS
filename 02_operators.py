8fe744323ea16fccb9c63b02c84d5d408b6efec0
Naveen Raghav <122202570+NaveenRaghav1@users.noreply.github.com>
2023-02-01 20:28:17 +0530

    Add files via upload

---
+++ 02_operators.py
@@ -0,0 +1,36 @@
+#arthmetic operations
+x=10
+y=20
+sum=x+y
+sub=x-y
+prod=x*y
+div=x/y
+print("the following operations are artimatic operations ")
+print(sum)
+print(sub)
+print(prod)
+print(div)
+#assignment operators
+a=100
+b=20
+a+=10
+b-=10
+a*=2
+a/=2
+print(a)
+print(b)
+# comparision operators
+n=10
+m=30
+c=n>m
+d=n<m
+e=n>=m
+print(c)
+print(d)
+print(e)
+#logical operators
+i= True
+j= False
+print("the given relation is ",i and j)
+print("the given relation is ",i or j)
+print("the given relation is ",not i)
