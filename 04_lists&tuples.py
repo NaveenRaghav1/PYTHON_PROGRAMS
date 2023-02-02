d0f472795ddcbf06aee1fa77ccdd13e2d2a3b984
Naveen Raghav <122202570+NaveenRaghav1@users.noreply.github.com>
2023-02-02 19:21:07 +0530

    Add files via upload

---
+++ 04_lists&tuples.py
@@ -0,0 +1,9 @@
+a1=input("enter the fruits name 1-")
+a2=input("enter the fruits name 2")
+a3=input("enter the fruits name 3")
+a4=input("enter the fruits name 4")
+a5=input("enter the fruits name 5")
+a6=input("enter the fruits name 6")
+
+fruits=[a1,a2,a3,a4,a5,a6]
+print(fruits)
---
+++ string_funct_test.py
@@ -0,0 +1,10 @@
+name="naveen"
+#print(name.replace("a","i"))
+a='''aniket is my\n younger\t brother 
+he is\' studies in 9th standard\f in modern public\\ school bulandshahr'''
+print(a)
+b="\x75"
+print(b)
+c="\ooo345"
+print(c)
+print("Tools\bQA\b")
