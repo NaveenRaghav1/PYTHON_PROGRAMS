a7d86dda9a4c8a26ce2e4e5517c4c2b9a7d8ccd1
Naveen Raghav <122202570+NaveenRaghav1@users.noreply.github.com>
2023-02-07 19:47:52 +0530

    Add files via upload

---
+++ for_loop.py
@@ -0,0 +1,10 @@
+for i in range(1,10): #for loop easy program
+   # print(i)
+    
+    if i<10:
+            print(i)
+    elif i==9:
+        break
+    else:
+        print("invalid number")
+        break
