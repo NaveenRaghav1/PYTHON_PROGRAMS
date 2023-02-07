0ed8c6e39b8f010577821729a0f855b9a7f7e799
Naveen Raghav <122202570+NaveenRaghav1@users.noreply.github.com>
2023-02-07 19:48:26 +0530

    Add files via upload

---
+++ natural_no.py
@@ -0,0 +1,14 @@
+#print natural numbers using while loop 
+''' i=1
+while i<=10:
+    print(i)
+    i=i+1
+    
+    
+    '''
+i=1
+n=int(input("enter a number "))
+
+while i<=n:
+     print(i)
+     i+=1
