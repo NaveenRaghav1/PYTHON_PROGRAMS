bd6d73ea8c42c8af3def5600ee391cb5b9c75c75
Naveen Raghav <122202570+NaveenRaghav1@users.noreply.github.com>
2023-02-08 23:19:55 +0530

    Add files via upload

---
+++ variables_assign.py
@@ -0,0 +1,16 @@
+#assign multiple values to multiple variables.....
+a, b, c="naveen", "payal", "aniket"
+print(a)
+print(b)
+print(c)
+
+#one value to multiple variables....
+a=b=c="RAGHAV"
+print(a)
+
+#unpack a collection
+clothes=["kurta","pajama","shirts","pants","coat"]
+x,y,z,w,q=clothes
+print(x)
+print(y)
+print(z)
