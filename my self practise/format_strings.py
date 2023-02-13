2ebbbaebe80ced2fc732cb69f0ad536b18920835
Naveen Raghav <122202570+NaveenRaghav1@users.noreply.github.com>
2023-02-13 21:15:29 +0530

    Add files via upload

---
+++ my self practise/format_strings.py
@@ -0,0 +1,9 @@
+'''car_no=4543
+bolero="we have a lucky bolero with no. {}"
+print(bolero.format(car_no))
+'''
+price=100
+quantity=7
+items=10
+order="i want item with number{} on the price of {},in qauantity{}"
+print(order.format(items,price,quantity))
---
+++ my self practise/string.method.py
@@ -0,0 +1,5 @@
+name="NAVEEN"
+a="hello guys i am naveen raghav and i am studying in gla university"
+print(a.count("i"))
+print(name.casefold())
+print(name.center(30))
