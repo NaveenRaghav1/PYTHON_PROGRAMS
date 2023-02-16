7a2c7c5ad34495bf185e9d73d59ef4891cbb3954
Naveen Raghav <122202570+NaveenRaghav1@users.noreply.github.com>
2023-02-16 18:17:05 +0530

    Add files via upload

---
+++ Basics Programs/if_else_prog.py/pick_apple.py
@@ -0,0 +1,6 @@
+n=int(input("enter the no of apples to be picked--"))
+if n%7==0:
+    print("pick all the apples")
+else:
+    print("1 apple is left behind")
+print("minimum numbers of apples left he has--",n+1)    
---
+++ Basics Programs/if_else_prog.py/subtract_smaller.py
@@ -0,0 +1,6 @@
+num1=int(input("enter  first number--"))
+num2=int(input("enter second number--"))
+if num1<num2:
+    print("subtractionn between number is --",num2-num1)
+else:
+    print("subtractionn between number is --",num1-num2)
