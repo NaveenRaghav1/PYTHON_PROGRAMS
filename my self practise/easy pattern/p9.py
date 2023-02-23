374cce9398fe913d955e74e343681c7ebdd3daaf
Naveen Raghav <122202570+NaveenRaghav1@users.noreply.github.com>
2023-02-23 19:23:48 +0530

    Add files via upload

---
+++ my self practise/easy pattern/p9.py
@@ -0,0 +1,9 @@
+#n=int(input())
+for i in range(7):
+    if i==2 or i==4:
+        print("*",end="")
+    for j in range(7):
+        if i==2 or i==4:
+            print("*",end="")
+    print()
+    #wrong
