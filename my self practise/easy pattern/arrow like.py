e6d920b4fea297d1aa09f6a83cb3823b5e5d3e71
Naveen Raghav <122202570+NaveenRaghav1@users.noreply.github.com>
2023-03-05 22:11:21 +0530

    Add files via upload

---
+++ my self practise/easy pattern/arrow like.py
@@ -0,0 +1,13 @@
+n=int(input())
+#
+for i in range(n):
+    for j in range(i+1):
+        print("*",end=' ')
+    print("\r")
+    
+for i in range (n,0,-1):
+    for j in range(0,i-1):
+        print("*",end=' ')
+    print("\r")
+
+    
