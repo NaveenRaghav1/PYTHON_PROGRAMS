9b53647927a7b8f4469e02bb8c2fa8f4f5320375
Naveen Raghav <122202570+NaveenRaghav1@users.noreply.github.com>
2023-04-13 21:50:12 +0530

    Add files via upload

---
+++ Revision1.py
@@ -0,0 +1,15 @@
+#FIBONACCI SERIES......😍😍😍
+n= input("enter the no till you find the range of fibonaacci series--")
+a=int(n)
+n1,n2=0,1
+count=0
+while count<a:
+    print(n1)#for including 0th term
+    c=n1+n2 #for increasse the value
+    n1=n2#for swapping the numbers
+    n2=c
+    count+=1
+    
+    
+
+    
