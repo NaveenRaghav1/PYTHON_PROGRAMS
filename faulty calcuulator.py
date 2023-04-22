e5f12f471b9d5fd499b8610866c0a3d8df91d7f4
Naveen Raghav <122202570+NaveenRaghav1@users.noreply.github.com>
2023-04-22 17:55:15 +0530

    Add files via upload

---
+++ faulty calcuulator.py
@@ -0,0 +1,26 @@
+#faulty calculator ........
+
+n=int(input('enter first number=='))
+m=int(input('enter second number=='))
+op=input("enter the operands(*,/,+,-) waht you performe betwee them.....-")
+if n==45 and m==9 and op=='*':
+    print('555')
+elif op=='*':
+    print('product is ',n*m)
+if op=='-':
+    print('subtraction is ',n-m)
+if n==56 and m==9 and op=='+':
+    print('77')
+elif op=='+':
+    print('addition is ',n+m)
+if n==56 and m==6 and op=='/':
+    print('4')
+elif op=='/':
+    print('division is ',n/m)
+
+   
+    
+    
+ 
+    
+    
