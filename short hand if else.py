d8c8efb836b1d38e99b7b32ef6eaaf9cb5838a58
Naveen Raghav <122202570+NaveenRaghav1@users.noreply.github.com>
2023-04-23 18:51:31 +0530

    Add files via upload

---
+++ short hand if else.py
@@ -0,0 +1,7 @@
+a=int(input("enter your age-"))
+print("you can drive ") if a>18 else print("you can not drive")
+
+
+
+age=int(input("enter you age -->"))
+print("you are child..") if age<10  else print("you are adult--") 
