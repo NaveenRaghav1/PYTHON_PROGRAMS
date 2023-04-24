edd256054b45b00cdec915f75aedcabdb9eb1a27
Naveen Raghav <122202570+NaveenRaghav1@users.noreply.github.com>
2023-04-24 17:09:36 +0530

    Add files via upload

---
+++ loop_Concept.py
@@ -0,0 +1,9 @@
+while 1:
+    n=int(input("enter the number=\n"))
+    if n%2==0:
+        print("entered number is even .")
+        print("thnks for trying me")
+        break
+    else:
+        print('try again☹️ ☹️-entered number is not even-- ')
+        continue
