1654686222597309efb4daa718439d7a4f1bf23b
Naveen Raghav <122202570+NaveenRaghav1@users.noreply.github.com>
2023-05-01 19:35:43 +0530

    Add files via upload

---
+++ Hangman.py
@@ -0,0 +1,33 @@
+import random as r
+list=['tiger','lion','elephant','giraffe','bear','monkey']
+re=r.choice(list)
+mis_word=''
+mis_letter=input('abcdefghijklmnopqrstuvwxyz')
+guess=0
+while guess<10:
+    w=input("Please guess a letter-->")
+    guess+=1
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+print('''
+      +---+
+      o   |
+    / | \ |
+    /   \ |
+         === 
+      ''')
