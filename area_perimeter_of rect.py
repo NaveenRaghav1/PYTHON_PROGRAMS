9aaaf894904cd26e2a760c6ebccb1440431ea8fe
Naveen Raghav <122202570+NaveenRaghav1@users.noreply.github.com>
2023-06-22 12:18:12 +0530

    Add files via upload

---
+++ area_perimeter_of rect.py
@@ -0,0 +1,12 @@
+#area and perimeter of the rectangle
+#input section
+len=int(input("enter the length of the rectangle-"))
+bdth=int(input("enter the breadth of the rectangle-"))
+
+#logic section
+perimeter=len+bdth
+area=len*bdth
+
+#output section
+print("perimeter of the rectangle is =",perimeter)
+print("area of the rectangle =",area)
