e697dd921f6176098c9897f0f9aa311a3b70ebdd
Naveen Raghav <122202570+NaveenRaghav1@users.noreply.github.com>
2024-05-06 04:36:31 +0530

    Add files via upload

---
+++ PYTHON LAB/Dice.py
@@ -0,0 +1,13 @@
+import random as r
+name=input("enter your name please-->\n")
+print("-------------------------------------------")
+print('''           
+      -🙏🙏-------WELCOME-------🙏🙏-
+                    ''')
+print(name,'! Lets begin the game...😊😊 ')
+print("-------------------------------------------")
+import random
+
+ls = [1,2,3,4,5,6]
+
+print(random.choice(ls))
